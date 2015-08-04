5ade71f9363728288ed2b3e5aacf9f3c9079074d
Adnan-Sivro <asivro1@etf.unsa.ba>
2015-08-04 12:06:41 +0200

    first commit

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PCL Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Write to PLC";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(171, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Read from PLC";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PLC Adress";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Trenutna vrijednost temp";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(361, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(19, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 92);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Konekcija";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication2.Properties.Resources.rsz_sch_modicon_m241;
+            this.pictureBox1.Location = new System.Drawing.Point(518, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 171);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 257);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+using System.IO.Ports;
+using MelsecPLC;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private int counter = 0;
+       private List<string> linije = File.ReadLines("C:\\Users\\Korisnik\\Documents\\visual studio 2012\\Projects\\WindowsFormsApplication2\\WindowsFormsApplication2\\tekst.txt").ToList();
+       private int brojac = 0;
+        public MelsecPLC.Winsock winsock1;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (winsock1.GetState.ToString() != "Connected")
+                {
+
+                    winsock1Connect();
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+            /*DateTime trenutno = DateTime.Now;
+            string tren = trenutno.ToString("HH:mm:ss");
+            string sat = tren.Substring(0, 2);
+            string minuta = tren.Substring(3, 2);
+            string sekunda = tren.Substring(6, 2);
+
+            System.IO.StreamWriter tekst = new System.IO.StreamWriter("C:\\Users\\Korisnik\\Documents\\visual studio 2012\\Projects\\WindowsFormsApplication2\\WindowsFormsApplication2\\tekst.txt", true);
+            List<string> linije = File.ReadLines("C:\\Users\\Korisnik\\Documents\\visual studio 2012\\Projects\\WindowsFormsApplication2\\WindowsFormsApplication2\\tekst.txt").ToList();
+
+            foreach (string lajn in linije) {
+                string[] parametri = lajn.Split(' ');
+                if (Convert.ToInt32(parametri[0]) < Convert.ToInt32(sat) && Convert.ToInt32(parametri[1]) > Convert.ToInt32(sat)) {
+                    label2.Text = parametri[2];
+                }
+            }
+               */ 
+           
+            
+            
+
+            /* winsock1Connect();
+            winsock1.DataArrival += new MelsecPLC.Winsock.DataArrivalEventHandler(winsock1_DataArrival);
+            timer1.Enabled = true;
+            timer1.Start();*/
+           
+        }
+
+        private void winsock1Connect()
+        {
+            try
+            {
+                if (winsock1.GetState.ToString() != "Connected")
+                {
+
+                    winsock1.LocalPort = 1027;
+
+                    winsock1.RemoteIP = textBox1.Text.Trim();
+                    int a = 8000;
+
+                    winsock1.RemotePort = 8000;
+
+                    winsock1.Connect();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (winsock1.GetState.ToString() != "Connected")
+            {
+                winsock1Connect();
+            }
+            //String cmd = "500000FF03FF000018000A04010000D*0095000001";
+            String cmd = "";
+            String OutAddress = "0001";
+            cmd = "";
+            cmd = cmd + "5000";// sub HEAD (NOT)
+            cmd = cmd + "00";//   network number (NOT)
+            cmd = cmd + "FF";//PLC NUMBER
+            cmd = cmd + "03FF";// DEMAND OBJECT MUDULE I/O NUMBER
+            cmd = cmd + "00";//  DEMAND OBJECT MUDULE DEVICE NUMBER
+            cmd = cmd + "001C";//  Length of demand data
+            cmd = cmd + "000A";//  CPU inspector data
+            cmd = cmd + "0401";//  Read command
+            cmd = cmd + "0000";//  Sub command
+            cmd = cmd + "D*";//   device code
+            cmd = cmd + "009500"; //adBase 
+            cmd = cmd + OutAddress;  //BASE ADDRESS           
+            winsock1.Send(cmd);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*if (winsock1.GetState.ToString() != "Connected")
+            {
+                winsock1Connect();
+            }
+            String cmd = "";
+            String OutAddress = txtWrite.Text.Trim();
+            cmd = "";
+            cmd = cmd + "5000";// sub HEAD (NOT)
+            cmd = cmd + "00";//   network number (NOT)
+            cmd = cmd + "FF";//PLC NUMBER
+            cmd = cmd + "03FF";// DEMAND OBJECT MUDULE I/O NUMBER
+            cmd = cmd + "00";//  DEMAND OBJECT MUDULE DEVICE NUMBER
+            cmd = cmd + "001C";//  Length of demand data
+            cmd = cmd + "000A";//  CPU inspector data
+            cmd = cmd + "1401";//  Write command
+            cmd = cmd + "0000";//  Sub command
+            cmd = cmd + "D*";//   device code
+            cmd = cmd + "009501"; //adBase 
+            cmd = cmd + OutAddress;  //BASE ADDRESS
+            winsock1.Send(cmd);
+        }*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            string[] parametri = linije[0].Split(' ');
+            label2.Text = parametri[1];
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] parametri = linije[brojac].Split(' ');
+            label3.Text = (counter+1).ToString();
+            counter++;
+            if (brojac + 1 < linije.Count)
+            {
+                if (counter == Convert.ToInt32(parametri[0]))
+                {
+                    brojac++;
+                    counter = 0;
+                    string[] parametri1 = linije[brojac].Split(' ');
+
+                    label2.Text = parametri1[1];
+                }
+            }
+            else
+            {
+                timer1.Stop();
+
+                MessageBox.Show("Nema više linija");
+            }
+
+        }
+    }
+}
